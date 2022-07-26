135719bf9d828d876ebf567023ee617930136118
AkshayParihar <akshay.parihar03@gmail.com>
2022-07-26 14:05:47 +0530

    Adding postconstruct to print properties based on profile

--- restful/src/main/java/com/springboot/restful/RestfulApplication.java
+++ restful/src/main/java/com/springboot/restful/RestfulApplication.java
@@ -1,21 +1,35 @@
 package com.springboot.restful;
 
+import com.springboot.restful.configuration.ProfileManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class RestfulApplication {
 
+	@Autowired
+	ProfileManager profileManager;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestfulApplication.class, args);
+
 	}
 
 	@Bean
 	public RestTemplate getTemplate(RestTemplateBuilder builder){
 		return builder.build();
+	}
+
+	@PostConstruct
+	public void showProfileManager() {
+		profileManager.getPropertyValue();
 	}
 
 }
