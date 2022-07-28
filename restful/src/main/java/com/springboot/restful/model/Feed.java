4a5621f59cbdcd7d34181404ddda0623a44c20af
AkshayParihar <akshay.parihar03@gmail.com>
2022-07-29 01:40:55 +0530

    Adding JsonInclude annotation to include only non-null fields

--- restful/src/main/java/com/springboot/restful/model/Feed.java
+++ restful/src/main/java/com/springboot/restful/model/Feed.java
@@ -1,10 +1,12 @@
 package com.springboot.restful.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Feed {
     @Getter
     @Setter
