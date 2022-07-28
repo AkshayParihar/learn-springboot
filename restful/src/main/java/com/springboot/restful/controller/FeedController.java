9d9d67711b9de5bba09977e43e1b5f6bf1228ffc
AkshayParihar <akshay.parihar03@gmail.com>
2022-07-29 01:42:47 +0530

    updating controller with post mapping to create feeds

--- restful/src/main/java/com/springboot/restful/controller/FeedController.java
+++ restful/src/main/java/com/springboot/restful/controller/FeedController.java
@@ -2,10 +2,13 @@ package com.springboot.restful.controller;
 
 import com.springboot.restful.model.Feed;
 import com.springboot.restful.service.GetFeedsService;
+import com.springboot.restful.service.PostFeedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Map;
 
 @RestController
 public class FeedController {
@@ -13,8 +16,19 @@ public class FeedController {
     @Autowired
     GetFeedsService getFeedsService;
 
+    @Autowired
+    PostFeedService postFeedService;
+
     @GetMapping("/feeds")
     public ResponseEntity<Feed[]> getFeeds(){
         return ResponseEntity.ok().body(getFeedsService.getFeeds());
+    }
+
+    @PostMapping("/feed")
+    public ResponseEntity<Feed> postFeed(
+            @RequestBody Feed feed,
+            @RequestHeader Map<String,String> headers){
+        Feed responseBody = postFeedService.postFeed(headers,feed);
+        return ResponseEntity.created(URI.create("/feeds/"+feed.getUserId())).body(responseBody);
     }
 }
